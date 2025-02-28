4f46721e950cba8039a5592da3cbec408170150a
S-MADHAN-KUMAR <madhankumar4195@gmail.com>
2025-02-28 09:32:01 +0530

    first commit

---
+++ BASIC_SQL.sql
@@ -0,0 +1,21 @@
+-- CREATE DATABASE name_of_the_db       "To create DB"
+
+-- USE name_of_the_db                   " Select the DB"
+
+-- CREATE TABLE name_of_the_table(
+    -- id INT PRIMARY KEY AUTO_INCREMENT,
+    -- name VARCHR(10),                                        " to create Table  "
+    -- email VARCHAR(30),
+    -- create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- )      
+
+-- SHOW DATABASE              "Checking available db"
+
+-- DROP DATABASE name_of_the_db         "Delete the db"
+
+-- DROP TABLE name_of_the_db         "Delete the table"
+
+-- SHOW TABLES                     "Checking available tables"
+
+-- DROP TABLE IF EXISTS name_of_the_table    "Checking if the table exists before drop or delete "
+
